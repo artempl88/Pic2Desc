0c298c9be3e2ca0218921cd201f7e5e67abde378
artempl88 <artempl88@mail.ru>
2023-01-29 16:32:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import telegram
+import json
 
 TOKEN = "your_bot_token"
 
@@ -8,19 +9,30 @@ def handle_photo(update, context):
     photo_file = update.message.photo[-1].get_file()
     photo_file.download('image.jpg')
     
+    # Prepare the image for TensorFlow API
+    image = open('image.jpg', 'rb').read()
+    headers = {'Content-Type': 'application/octet-stream'}
+    data = {'signature_name': 'serving_default'}
+    url = 'TensorFlow_API_URL'
+
     # Send the image to TensorFlow API
-    response = requests.post('TensorFlow_API_URL', files={'image': open('image.jpg', 'rb')})
+    response = requests.post(url, headers=headers, data=image, json=data)
     result = response.json()
     
     # Assign the result to a variable 'I'
     I = result["info"]
     
-    # Send the result to ChatGPT API
-    response = requests.post('ChatGPT_API_URL', data={'text': I})
+    # Prepare the request for ChatGPT API
+    headers = {'Content-Type': 'application/json'}
+    data = {'prompt': I, 'max_tokens': 100}
+    url = 'ChatGPT_API_URL'
+
+    # Send the request to ChatGPT API
+    response = requests.post(url, headers=headers, data=json.dumps(data))
     reply = response.json()
     
     # Reply to the user with the result from ChatGPT
-    context.bot.send_message(chat_id=update.message.chat_id, text=reply["output"])
+    context.bot.send_message(chat_id=update.message.chat_id, text=reply["choices"][0]["text"])
 
 # Initialize the bot and add the photo handler
 bot = telegram.Bot(TOKEN)
