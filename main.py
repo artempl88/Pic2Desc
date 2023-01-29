f5cf6c67e818c32b70a9d8f193e559dad4e677c4
artempl88 <artempl88@mail.ru>
2023-01-29 16:53:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,68 @@
 import requests
-import telegram
 import json
+import tensorflow as tf
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "your_bot_token"
+# Enter your Telegram Bot API Key
+API_KEY = "your_telegram_bot_api_key"
 
-def handle_photo(update, context):
+# Enter your TensorFlow API Key
+TF_API_KEY = "your_tensorflow_api_key"
+
+# Enter your OpenAI API Key
+OPENAI_API_KEY = "your_openai_api_key"
+
+def process_image(image_file):
+    # Process the image using TensorFlow API
+    # Replace this with your code to process the image using TensorFlow
+    # ...
+    return processed_data
+
+def generate_description(data):
+    # Generate a description using the OpenAI API
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY
+    }
+    model = "text-davinci-002"
+    prompt = "Write a selling product description for the site " + data
+    completions = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Send me a photo of the product and I will generate a description for it.")
+
+def image_received(update, context):
     # Get the image file from the update
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('image.jpg')
-    
-    # Prepare the image for TensorFlow API
-    image = open('image.jpg', 'rb').read()
-    headers = {'Content-Type': 'application/octet-stream'}
-    data = {'signature_name': 'serving_default'}
-    url = 'TensorFlow_API_URL'
+    image_file = update.message.photo[-1].get_file()
+    image_file.download("product.jpg")
 
-    # Send the image to TensorFlow API
-    response = requests.post(url, headers=headers, data=image, json=data)
-    result = response.json()
-    
-    # Assign the result to a variable 'I'
-    I = result["info"]
-    
-    # Prepare the request for ChatGPT API
-    headers = {'Content-Type': 'application/json'}
-    prompt = "Write a selling product description for the site: " + I
-    data = {'prompt': prompt, 'max_tokens': 100}
-    url = 'ChatGPT_API_URL'
+    # Process the image and generate a description
+    processed_data = process_image("product.jpg")
+    description = generate_description(processed_data)
 
-    # Send the request to ChatGPT API
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    reply = response.json()
-    
-    # Reply to the user with the result from ChatGPT
-    context.bot.send_message(chat_id=update.message.chat_id, text=reply["choices"][0]["text"])
+    # Send the description back to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=description)
 
-# Initialize the bot and add the photo handler
-bot = telegram.Bot(TOKEN)
-updater = telegram.Updater(TOKEN, use_context=True)
+# Initialize the Telegram Bot
+updater = Updater(token=API_KEY, use_context=True)
 dispatcher = updater.dispatcher
-dispatcher.add_handler(telegram.MessageHandler(telegram.MessageType.PHOTO, handle_photo))
+
+# Add handlers for the bot
+start_handler = CommandHandler("start", start)
+dispatcher.add_handler(start_handler)
+
+image_handler = MessageHandler(Filters.photo, image_received)
+dispatcher.add_handler(image_handler)
 
 # Start the bot
 updater.start_polling()
