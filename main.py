f8bf21351899328e38f2abe0bad25c908b6f2654
Artem Pluzhnikov <artempl88@mail.ru>
2023-02-01 15:08:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,94 @@
+import telebot
 import requests
-import tensorflow as tf
-import telegram
+import json
 
-# Enter your Telegram bot token
-bot_token = "YOUR_BOT_TOKEN"
+bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 
-# Enter your TensorFlow API key
-tf_api_key = "YOUR_TF_API_KEY"
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(chat_id=message.chat.id, text="How many characters should be in the product description?")
+    bot.register_next_step_handler(message, process_n_step)
 
-# Enter your ChatGPT API key
-gpt_api_key = "YOUR_GPT_API_KEY"
+def process_n_step(message):
+    try:
+        n = int(message.text)
+        bot.send_message(chat_id=message.chat.id, text="Please take a nice picture of the object.")
+        bot.register_next_step_handler(message, process_photo_step, n)
+    except:
+        bot.send_message(chat_id=message.chat.id, text="Invalid input. Please enter a valid number.")
+        bot.register_next_step_handler(message, process_n_step)
 
-# Enter your Yandex Translate API key
-translate_api_key = "YOUR_TRANSLATE_API_KEY"
+def process_photo_step(message, n):
+    # Receive the photo and pass it to the Vision handler
+    photo = message.photo[-1].file_id
+    vision_url = "https://vision.mail.ru/api/v1/recognition"
+    data = {
+        "api_key": "YOUR_VISION_API_KEY",
+        "model_id": "YOUR_MODEL_ID",
+        "file_id": photo,
+        "type": "object"
+    }
+    response = requests.post(vision_url, data=data)
+    result = json.loads(response.text)
+    # Extract the noun and adjective from the result
+    item = result["objects"][0]["name"] + " " + result["objects"][0]["color"]
+    # Pass the item information to the Chat GPT API
+    chat_gpt_url = "https://chat-gpt.openai.com/v1/engines/YOUR_CHAT_GPT_ENGINE/messages"
+    data = {
+        "prompt": "Write a selling description for the site of the following product: " + item,
+        "max_tokens": n,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.5
+    }
+    response = requests.post(chat_gpt_url, data=data)
+    result = json.loads(response.text)
+    desc = result["choices"][0]["text"]
+    # Translate the description to Russian
+    yandex_translator_url = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+    data = {
+        "key": "YOUR_YANDEX_TRANSLATOR_API_KEY",
+        "text": desc,
+        "lang": "en-ru"
+    }
+    response = requests.post(yandex_translator_url, data=data)
+    result = json.loads(response.text)
+    desc = result["text"][0]
+    # Send the translated description back to the user
+    bot.send_message(chat_id=message.chat.id, text=desc)
+    # Show the buttons
+    place_product_btn = InlineKeyboardButton("Place the product", callback_data="place_product")
+    generate_more_btn = InlineKeyboardButton("Generate more", callback_data="generate_more")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [place_product_btn, generate_more_btn]
+    ])
+    bot.send_message(chat_id=message.chat.id, text="What would you like to do next?", reply_markup=keyboard)
 
-# Create a Telegram bot
-bot = telegram.Bot(token=bot_token)
-
-# Define the handler function for incoming messages
-def handle_message(message):
-    # Check if the message contains a photo
-    if message.photo:
-        # Get the largest photo file
-        photo = bot.get_file(message.photo[-1].file_id)
-        photo.download('image.jpg')
-        
-        # Use TensorFlow to process the image and extract information
-        information = tf.process_image('image.jpg', api_key=tf_api_key)
-        
-        # Send the information to the ChatGPT API to generate a product description
-        response = requests.post(
-            "https://api.openai.com/v1/engines/davinci/jobs",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {gpt_api_key}"
-            },
-            json={
-                "prompt": "Write a selling product description for the site: " + information,
-                "max_tokens": 1024
-            }
-        )
-        
-        # Extract the product description from the response
-        product_description = response.json()["choices"][0]["text"]
-        
-        # Translate the product description into Russian using the Yandex Translate API
-        response = requests.get(
-            f"https://translate.yandex.net/api/v1.5/tr.json/translate?key={translate_api_key}&text={product_description}&lang=ru"
-        )
-        translated_description = response.json()["text"][0]
-        
-        # Send the translated description back to the user
-        bot.send_message(chat_id=message.chat.id, text=translated_description)
-
-# Start the bot
-bot.set_update_listener(handle_message)
+def on_callback_query(callback_query):
+    if callback_query.data == "place_product":
+    # Place the product on the site using the Woocommerce API
+    # Place the photo and description
+    # Show the message that the product has been successfully placed
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer YOUR_WOOCOMMERCE_API_KEY"
+    }
+    data = {
+    "name": item,
+    "description": desc,
+    "images": [{
+        "src": photo_url
+    }],
+    "price": "$10.00"
+    }
+    woocommerce_api_url = "https://YOUR_WOOCOMMERCE_STORE_URL/wp-json/wc/v3/products"
+    response = requests.post(woocommerce_api_url, headers=headers, data=json.dumps(data))
+    bot.answer_callback_query(callback_query.id, text="Product has been successfully placed on the site!")
+    elif callback_query.data == "generate_more":
+    # Return to Step 8
+    # Pass the information to the Chat GPT API and get the description
+    # Translate the description using the Yandex Translator API
+    # Show the translated description to the user
+    # Show the buttons again
+    # Repeat the process
 bot.polling()
